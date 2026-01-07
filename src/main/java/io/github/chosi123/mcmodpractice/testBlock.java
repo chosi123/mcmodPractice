71769f7fab871a156971268c2ed8e54701c23a16
CHOSI123\fyg75 <fyg7575@gmail.com>
2026-01-07 22:37:29 +0900

    add testblock class

---
+++ src/main/java/io/github/chosi123/mcmodpractice/testBlock.java
@@ -0,0 +1,9 @@
+package io.github.chosi123.mcmodpractice;
+
+import net.minecraft.world.level.block.Block;
+
+public class testBlock extends Block {
+    public testBlock(Properties properties) {
+        super(properties);
+    }
+}
